d292dcdabe274839e57d86de7df4660bd646a326
Guillermo Musumeci <guillermo@kopicloud.com>
2023-03-31 22:18:10 +0200

    Initial commit

---
+++ ad-user-basic/user-create.tf
@@ -0,0 +1,25 @@
+# User Name
+resource "random_string" "random" {
+  length  = 3
+  special = false
+  upper   = false
+}
+
+// Create Basic User
+resource "kopicloud_user" "test_1" {
+  username     = "labtestuser-${random_string.random.result}"
+  password     = "P@ssword"
+  first_name   = "Lab Test"
+  initials     = "X"
+  last_name    = "User ${random_string.random.result}"
+  display_name = "Lab Test X User ${random_string.random.result}"
+  description  = "Lab Test X User ${random_string.random.result}"
+
+  change_password_next_logon = true
+  password_never_expires     = true
+}
+
+output "OUTPUT_new_user_1" {
+  description = "Created User 1"
+  value       = resource.kopicloud_user.test_1
+}
